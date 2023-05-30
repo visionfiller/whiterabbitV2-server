985d7b4057fcdf1118ce92d5498dee4a32ac080b
Vision Filler <visionfiller@gmail.com>
2023-05-30 14:37:42 -0500

    hi

--- whiterabbitapi/models/varietal.py
+++ whiterabbitapi/models/varietal.py
@@ -4,5 +4,5 @@ from django.db import models
 class Varietal(models.Model):
     name = models.CharField(max_length=155)
     image = models.CharField(max_length=155)
-    description = models.CharField(max_length=155)
+    description = models.CharField(max_length=500)
     wine_type =models.ForeignKey("WineType",on_delete=models.CASCADE)
