0751b0705476a9e01bde6216ea0d6706a7cfbf37
Vision Filler <visionfiller@gmail.com>
2023-05-31 12:56:29 -0500

    fixed cors headers

--- whiterabbit/settings.py
+++ whiterabbit/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://whiterabbit-client.herokuapp.com'
+    'https://whiterabbit-client.herokuapp.com',
     'https://white-rabbit.live'
 )
 
